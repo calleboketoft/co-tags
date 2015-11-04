81a12caebbbeb2608df71a364edbf6aaaa2402b6
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-11-04 10:49:16 +0100

    example styleing

--- src/example/app-cmp.ts
+++ src/example/app-cmp.ts
@@ -5,8 +5,10 @@ import { CoTagsCmp } from '../co-tags/co-tags-cmp'
   selector: 'co-tags-example',
   directives: [CoTagsCmp],
   template: `
-    <h3>co-tags</h3>
-    <co-tags [tags]="tags"></co-tags>
+    <div class="container" style="margin-top: 25px;">
+      <h3>co-tags</h3>
+      <co-tags [tags]="tags"></co-tags>
+    </div>
   `
 })
 export class AppCmp {
