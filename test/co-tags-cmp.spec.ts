3f7ef62ed6deb7e4d0578492b10aa62035e0e109
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-11-16 12:40:34 +0100

    3 proper tests

--- test/co-tags-cmp.spec.ts
+++ test/co-tags-cmp.spec.ts
@@ -2,8 +2,22 @@ import 'reflect-metadata'
 import 'zone.js'
 import { CoTagsCmp } from '../src/co-tags/co-tags-cmp'
 
-describe('CoTagsCmp', function() {
-  it('can add a new tag', function() {
-    expect(CoTagsCmp).not.toBe(undefined)
+describe('CoTagsCmp', () => {
+  it('should be defined', () => {
+    expect(CoTagsCmp).toBeDefined()
+  })
+
+  it('should be able to add new tags', () => {
+    let coTagsCmp = new CoTagsCmp()
+    coTagsCmp.tags = ['a', 'b']
+    coTagsCmp.addTag('c')
+    expect(coTagsCmp.tags).toEqual(['a', 'b', 'c'])
+  })
+
+  it('should be able to remove tags', () => {
+    let coTagsCmp = new CoTagsCmp()
+    coTagsCmp.tags = ['a', 'b', 'c']
+    coTagsCmp.removeTag('b')
+    expect(coTagsCmp.tags).toEqual(['a', 'c'])
   })
 })
